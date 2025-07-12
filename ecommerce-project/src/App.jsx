a9aebe7c9496f36443712b72dfddf422d3519e0c
Ontheball <bravelivesforever@gmail.com>
2025-07-12 15:23:03 +0300

    Move checkout page into React

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router'
 import HomePage from "./pages/HomePage";
+import CheckoutPage from './pages/CheckoutPage';
 import './App.css'
 
 export default function App() {
@@ -7,7 +8,7 @@ export default function App() {
     <>
       <Routes> 
         <Route path='/' element={<HomePage />}></Route>
-        <Route path='checkout' element={<div>Test</div>}></Route>
+        <Route path='checkout' element={<CheckoutPage />}></Route>
       </Routes>
     </>
   );
--- ecommerce-project/src/main.jsx
+++ ecommerce-project/src/main.jsx
@@ -1,16 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router' // without ./ means from node_modules
+import { BrowserRouter} from 'react-router' // without ./ means from node_modules
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route index element={<App />} />
-        <Route path='checkout' element={<div>this is checkout page</div>} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
